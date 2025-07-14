a462b6fb69059dd0b21cb57fdf5dc0220c1f1f0f
JUlian-co <hi@juliansinn.de>
2025-07-14 18:38:25 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+export function shortAddress(
+  address,
+  opts = {}
+) {
+  const left = opts.left ?? 2;
+  const right = opts.right ?? 4;
+
+  if (!address || address.length < left + right) return address;
+  return `${address.slice(0, left)}...${address.slice(-right)}`;
+}
